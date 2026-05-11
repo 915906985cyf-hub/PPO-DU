b4458d76a311aaeb307bb6185a51e0321ef22b27
915906985cyf-hub <915906985cyf@gmail.com>
2026-05-11 17:20:23 +0800

    Add files via upload

---
+++ replaybuffer.py
@@ -0,0 +1,43 @@
+import torch
+import numpy as np
+
+
+class ReplayBuffer:
+    def __init__(self, args):
+        self.buffer_capacity = args.batch_size
+        self.s = np.zeros((args.batch_size, args.state_dim))
+        self.a = np.zeros((args.batch_size, args.action_dim))
+        self.a_logprob = np.zeros((args.batch_size, args.action_dim))
+        self.r = np.zeros((args.batch_size, 1))
+        self.s_ = np.zeros((args.batch_size, args.state_dim))
+        self.dw = np.zeros((args.batch_size, 1))
+        self.done = np.zeros((args.batch_size, 1))
+        self.count = 0
+
+    def store(self, s, a, a_logprob, r, s_, dw, done):
+        if self.count < self.buffer_capacity:
+            self.s[self.count] = s
+            self.a[self.count] = a
+            self.a_logprob[self.count] = a_logprob
+            self.r[self.count] = r
+            self.s_[self.count] = s_
+            self.dw[self.count] = dw
+            self.done[self.count] = done
+            self.count += 1
+        else:
+            # 缓冲区已满，通常在这里触发更新，然后清空缓冲区
+            pass
+
+    def clear(self):
+        self.count = 0
+
+    def numpy_to_tensor(self):
+        s = torch.tensor(self.s, dtype=torch.float)
+        a = torch.tensor(self.a, dtype=torch.float)
+        a_logprob = torch.tensor(self.a_logprob, dtype=torch.float)
+        r = torch.tensor(self.r, dtype=torch.float)
+        s_ = torch.tensor(self.s_, dtype=torch.float)
+        dw = torch.tensor(self.dw, dtype=torch.float)
+        done = torch.tensor(self.done, dtype=torch.float)
+
+        return s, a, a_logprob, r, s_, dw, done
